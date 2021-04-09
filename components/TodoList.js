44b8059a08358bd67e0f870db994ce9ed13c7e32
heejukim-developer <heeju_a@naver.com>
2021-04-09 15:11:22 +0900

    Initial commit

---
+++ components/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
+import TodoItem from './TodoItem';
+
+const TodoListBlock = styled.div`
+ flex: 1;
+ /* 제일 많이 차지함 */
+ padding:20px 32px;
+ padding-bottom:48px;
+ overflow-y:auto;
+ /* background:gray; */
+`;
+
+function TodoList(){
+    const todos = useTodoState();
+
+    return (
+        <TodoListBlock>
+            {todos.map (todo => (
+             <TodoItem
+                key={todo.id}
+                id={todo.id}
+                text={todo.text}
+                done={todo.done}
+            />
+            ))}
+        </TodoListBlock>
+    );
+}
+
+export default TodoList;
